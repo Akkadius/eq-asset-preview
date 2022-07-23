a9266abe6c213b71f50dbdf0cad06c7980c83122
Chris Miles <akkadius1@gmail.com>
2022-07-23 15:16:41 -0500

    A new start

---
+++ Makefile
@@ -0,0 +1,67 @@
+#----------------------
+# Parse makefile arguments
+#----------------------
+RUN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+$(eval $(RUN_ARGS):;@:)
+
+#----------------------
+# Silence GNU Make
+#----------------------
+ifndef VERBOSE
+MAKEFLAGS += --no-print-directory
+endif
+
+#----------------------
+# Terminal
+#----------------------
+
+GREEN  := $(shell tput -Txterm setaf 2)
+WHITE  := $(shell tput -Txterm setaf 7)
+YELLOW := $(shell tput -Txterm setaf 3)
+RESET  := $(shell tput -Txterm sgr0)
+
+#------------------------------------------------------------------
+# - Add the following 'help' target to your Makefile
+# - Add help text after each target name starting with '\#\#'
+# - A category can be added with @category
+#------------------------------------------------------------------
+
+HELP_FUN = \
+	%help; \
+	while(<>) { \
+		push @{$$help{$$2 // 'options'}}, [$$1, $$3] if /^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
+		print "-----------------------------------------\n"; \
+		print "| Makefile menu\n"; \
+		print "-----------------------------------------\n"; \
+		print "| usage: make [command]\n"; \
+		print "-----------------------------------------\n\n"; \
+		for (sort keys %help) { \
+			print "${WHITE}$$_:${RESET \
+		}\n"; \
+		for (@{$$help{$$_}}) { \
+			$$sep = " " x (32 - length $$_->[0]); \
+			print "  ${YELLOW}$$_->[0]${RESET}$$sep${GREEN}$$_->[1]${RESET}\n"; \
+		}; \
+		print "\n"; \
+	}
+
+help: ##@other Show this help.
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
+
+#----------------------
+# build
+#----------------------
+
+build-maps: ##@build Builds JSON file maps
+	tree ./assets/objects -f -J --sort=name | jq -c > ./maps/objects-map.json
+	tree ./assets/npc_models -f -J --sort=name | jq -c > ./maps/npc-models-map.json
+	tree ./assets/monograms -f -J --sort=name | jq -c > ./maps/monograms-map.json
+	tree ./assets/item_icons -f -J --sort=name | jq -c > ./maps/item-icons-map.json
+	tree ./assets/spell_icons -f -J --sort=name | jq -c > ./maps/spell-icons-map.json
+	tree ./assets/spell_animations -f -J --sort=name | jq -c > ./maps/spell-animations-map.json
+	tree ./assets/expansion-icons-small -f -J --sort=name | jq -c > ./maps/expansion-icons-small-map.json
+	tree ./assets/wallpaper -f -J --sort=name | jq -c > ./maps/wallpapers.json
+	tree ./assets/emitters -f -J --sort=name | jq -c > ./maps/emitters.json
+	tree ./assets/player-animations -f -J --sort=name | jq -c > ./maps/player-animations.json
+	sed -i 's/"type":"file",//g' ./maps/*.json
+	@echo "Built maps!"
---
+++ main.go
@@ -0,0 +1,308 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adotout/pack_2d"
+	"github.com/nfnt/resize"
+	"image"
+	"image/draw"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+// spritesheet generator
+
+type config struct {
+	directory            string
+	iconNamePrefix       string
+	iconNameSuffix       string
+	spriteImageFile      string
+	spriteCssFile        string
+	spriteSize           uint    // exact size eg 15,30,50 [ ]
+	spritePercentageSize float32 // percentage of original .4 = 40%
+}
+
+const testHtmlTemplate = `<html>
+<head>
+    <link rel="stylesheet" href="{{spritesheet}}">
+</head>
+<body>
+{{sprites}}
+</body>
+</html>
+`
+
+func main() {
+	debug := len(os.Getenv("DEBUG")) > 0
+
+	// config
+	spriteSheets := []config{
+		{
+			directory:       "./assets/item_icons",
+			iconNamePrefix:  "", // comes from filename already
+			spriteImageFile: "./assets/sprites/item-icons.png",
+			spriteCssFile:   "./assets/sprites/item-icons.css",
+		},
+		{
+			directory:       "./assets/item_icons",
+			iconNamePrefix:  "", // comes from filename already
+			iconNameSuffix:  "-sm",
+			spriteImageFile: "./assets/sprites/item-icons-sm.png",
+			spriteCssFile:   "./assets/sprites/item-icons-sm.css",
+			spriteSize:      13, // needs to relatively match font size
+		},
+		{
+			directory:       "./assets/spell_icons",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-12",
+			spriteImageFile: "./assets/sprites/spell-icons-12.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-12.css",
+			spriteSize:      12,
+		},
+		{
+			directory:       "./assets/spell_icons",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-20",
+			spriteImageFile: "./assets/sprites/spell-icons-20.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-20.css",
+			spriteSize:      20,
+		},
+		{
+			directory:       "./assets/spell_icons",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-30",
+			spriteImageFile: "./assets/sprites/spell-icons-30.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-30.css",
+			spriteSize:      30,
+		},
+		{
+			directory:       "./assets/spell_icons",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-40",
+			spriteImageFile: "./assets/sprites/spell-icons-40.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-40.css",
+			spriteSize:      40,
+		},
+		// legacy spell icons
+		{
+			directory:       "./assets/spell_icons_legacy",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-12",
+			spriteImageFile: "./assets/sprites/spell-icons-legacy-12.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-legacy-12.css",
+			spriteSize:      12,
+		},
+		{
+			directory:       "./assets/spell_icons_legacy",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-20",
+			spriteImageFile: "./assets/sprites/spell-icons-legacy-20.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-legacy-20.css",
+			spriteSize:      20,
+		},
+		{
+			directory:       "./assets/spell_icons_legacy",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-30",
+			spriteImageFile: "./assets/sprites/spell-icons-legacy-30.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-legacy-30.css",
+			spriteSize:      30,
+		},
+		{
+			directory:       "./assets/spell_icons_legacy",
+			iconNamePrefix:  "spell-",
+			iconNameSuffix:  "-40",
+			spriteImageFile: "./assets/sprites/spell-icons-legacy-40.png",
+			spriteCssFile:   "./assets/sprites/spell-icons-legacy-40.css",
+			spriteSize:      40,
+		},
+		{
+			directory:       "./assets/objects",
+			iconNamePrefix:  "object-", // comes from filename already
+			spriteImageFile: "./assets/sprites/objects.png",
+			spriteCssFile:   "./assets/sprites/objects.css",
+		},
+		{
+			directory:       "./assets/spell_icons",
+			iconNamePrefix:  "spell-icon-",
+			spriteImageFile: "./assets/sprites/spell-icons.png",
+			spriteCssFile:   "./assets/sprites/spell-icons.css",
+		},
+		{
+			directory:       "./assets/npc_models",
+			iconNamePrefix:  "race-models-",
+			spriteImageFile: "./assets/sprites/race-models.png",
+			spriteCssFile:   "./assets/sprites/race-models.css",
+		},
+		{
+			directory:            "./assets/npc_models",
+			iconNamePrefix:       "race-models-",
+			iconNameSuffix:       "-sm",
+			spriteImageFile:      "./assets/sprites/race-models-sm.png",
+			spriteCssFile:        "./assets/sprites/race-models-sm.css",
+			spritePercentageSize: .50,
+		},
+		{
+			directory:            "./assets/faces",
+			iconNamePrefix:       "faces-",
+			spriteImageFile:      "./assets/sprites/faces.png",
+			spriteCssFile:        "./assets/sprites/faces.css",
+			spritePercentageSize: .25,
+		},
+	}
+
+	// loop through spriteSheets
+	for _, c := range spriteSheets {
+		dirName := c.directory
+
+		fmt.Printf("[%v] Scanning\n", c.directory)
+
+		dir, err := ioutil.ReadDir(dirName)
+		if err != nil {
+			panic(err)
+		}
+
+		packer := pack_2d.Packer2d{}
+		id := 0
+		images := map[int]image.Image{}
+		imageNames := map[int]string{}
+		for _, file := range dir {
+			if file.IsDir() {
+				continue
+			}
+			name := file.Name()
+			if !strings.HasSuffix(name, ".jpg") && !strings.HasSuffix(name, ".png") && !strings.HasSuffix(
+				name,
+				".gif",
+			) {
+				continue
+			}
+			imgReader, err := os.Open(filepath.Join(dirName, file.Name()))
+			if err != nil {
+				panic(err)
+			}
+			imgDecoded, _, err := image.Decode(imgReader)
+
+			// if sprite size specified, we are resizing the image
+			if c.spriteSize > 0 {
+				imgDecoded = resize.Resize(c.spriteSize, c.spriteSize, imgDecoded, resize.NearestNeighbor)
+			}
+
+			// if we are making the original image a percentage of the original
+			if c.spritePercentageSize > 0 {
+				w := float32(imgDecoded.Bounds().Dx()) * c.spritePercentageSize
+				h := float32(imgDecoded.Bounds().Dy()) * c.spritePercentageSize
+				imgDecoded = resize.Resize(uint(w), uint(h), imgDecoded, resize.NearestNeighbor)
+			}
+
+			packer.AddNewBlock(imgDecoded.Bounds().Max.X, imgDecoded.Bounds().Max.Y, id)
+			images[id] = imgDecoded
+			imageNames[id] = file.Name()
+			id++
+
+			imgReader.Close()
+		}
+
+		fmt.Printf("[%v] Found [%v] images...\n", c.directory, id)
+
+		packedImages, maxWidth, maxHeight := packer.Pack()
+		outImage := image.NewRGBA(image.Rect(0, 0, maxWidth, maxHeight))
+		css := ""
+		html := ""
+		for _, img := range packedImages {
+
+			// draw image
+			currentImage := images[img.Id]
+			mX := currentImage.Bounds().Max.X
+			mY := currentImage.Bounds().Max.Y
+			draw.Draw(outImage, image.Rect(img.X, img.Y, img.X+mX, img.Y+mY), currentImage, image.ZP, draw.Src)
+
+			// image properties
+			imageFileName := imageNames[img.Id]
+			imageName := slug(strings.TrimSuffix(imageFileName, filepath.Ext(imageFileName)))
+			imageHeight := (img.Y + mY) - img.Y
+			imageWidth := (img.X + mX) - img.X
+
+			// print out coordinates for debugging if need be
+			if debug {
+				fmt.Printf(
+					"[%v] X [%v] Y [%v] mX[%v] mY[%v] width [%v] height [%v]\n",
+					imageName,
+					img.X,
+					img.Y,
+					img.X+mX,
+					img.Y+mY,
+					imageWidth,
+					imageHeight,
+				)
+			}
+
+			css += fmt.Sprintf(
+				".%v { background: url('./%v') -%vpx -%vpx; height: %vpx; width: %vpx; display: inline-block; }\n",
+				fmt.Sprintf("%v%v%v", c.iconNamePrefix, imageName, c.iconNameSuffix),
+				filepath.Base(c.spriteImageFile),
+				img.X,
+				img.Y,
+				imageHeight,
+				imageWidth,
+			)
+			html += fmt.Sprintf(
+				"<span class=\"%v\"></span>\n",
+				fmt.Sprintf("%v%v%v", c.iconNamePrefix, imageName, c.iconNameSuffix),
+			)
+		}
+
+		// sprite
+		imgFile, _ := os.Create(c.spriteImageFile)
+		defer imgFile.Close()
+		png.Encode(imgFile, outImage)
+
+		fmt.Printf("[%v] Wrote [%v]\n", c.directory, c.spriteImageFile)
+
+		// css
+		f, err := os.Create(c.spriteCssFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+		_, err2 := f.WriteString(css)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		fmt.Printf("[%v] Wrote [%v]\n", c.directory, c.spriteCssFile)
+
+		// html
+		template := testHtmlTemplate
+		htmlFilePath := strings.ReplaceAll(c.spriteCssFile, "css", "html")
+		sheet := fmt.Sprintf("./%v", filepath.Base(c.spriteCssFile))
+		template = strings.ReplaceAll(template, "{{spritesheet}}", sheet)
+		template = strings.ReplaceAll(template, "{{sprites}}", html)
+
+		f, err = os.Create(htmlFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+		_, err2 = f.WriteString(template)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		fmt.Printf("[%v] Wrote [%v]\n", c.directory, htmlFilePath)
+	}
+
+}
+
+var re = regexp.MustCompile("[^a-z0-9]+")
+
+func slug(s string) string {
+	return strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+}
